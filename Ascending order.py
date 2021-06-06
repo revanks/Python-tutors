04b895e36c82b59a8055a765f2e6bdfb267d5329
revanks <39840999+revanks@users.noreply.github.com>
2021-06-06 18:12:44 +0530

    Add files via upload

---
+++ Ascending order.py
@@ -0,0 +1,14 @@
+i=int(input())
+b=list(map(int, input().split() ))
+m=b.copy()
+c=0
+for k in range(1, i):
+    if b==m.sort:
+        break
+    elif b[0]>b[1]:
+        b.remove(b[k-1])
+    elif b[-1]<b[-2]:
+        b.remove(b[-1])
+        c+=1
+print(c)
+    
